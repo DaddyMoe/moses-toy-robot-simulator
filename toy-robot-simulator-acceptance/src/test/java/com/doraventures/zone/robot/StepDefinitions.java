f1ca14f0baf0c470d7c083f51fa40141adbd409f
Moses Mansaray <moses@doraventures.com>
2018-05-23 17:22:46 +0100

    Adds in Cucumber feature stubs

--- toy-robot-simulator-acceptance/src/test/java/com/doraventures/zone/robot/StepDefinitions.java
+++ toy-robot-simulator-acceptance/src/test/java/com/doraventures/zone/robot/StepDefinitions.java
@@ -1,8 +1,36 @@
 package com.doraventures.zone.robot;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 /**
  * @author moses@doraventures.com
  */
 public class StepDefinitions extends SpringIntegrationBase {
 
+//  Given("^a robot placement of (\\d+) xAxis, (\\d+) yAxis and facing \"([^\"]*)\"$", (Integer xAxis, Integer yAxis, String reportExpected) -> {
+  @Given("^a robot placement of (\\d+) xAxis, (\\d+) yAxis and facing \"([^\"]*)\"$")
+    public void theSuppliedReferenceData(Integer xAxis, Integer yAxis, String reportExpected) throws Throwable {
+
+    System.out.println(xAxis);
+    System.out.println(yAxis);
+    System.out.println(reportExpected);
+    throw new PendingException();
+  }
+
+  @When("^I \"([^\"]*)\" the robot$")
+  public void iTheRobot(String moveCommand) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    System.out.println(moveCommand);
+    throw new PendingException();
+  }
+
+  @Then("^\"([^\"]*)\" should print \"([^\"]*)\"$")
+  public void shouldPrint(String reportCommand, String expectedOutput) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    System.out.println(reportCommand + " - " + expectedOutput);
+    throw new PendingException();
+  }
 }
--- toy-robot-simulator-acceptance/src/test/resources/feature_request.feature
+++ toy-robot-simulator-acceptance/src/test/resources/feature_request.feature
@@ -1,6 +1,6 @@
-Feature:
+Feature: Move Robot within table bounds
 
-  Scenario:
-    Given
-    When
-    Then
+  Scenario: Place and move Robot
+    Given a robot placement of 0 xAxis, 0 yAxis and facing "North"
+    When I "MOVE" the robot
+    Then "REPORT" should print "Output: 0,1,NORTH"
